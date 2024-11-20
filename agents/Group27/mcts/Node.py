d850963cb99dd7084dbe6ea05a0a05171bd548a3
Minetailor <rtaylortagb@gmail.com>
2024-11-20 19:55:24 +0000

    Remove has_ended from simulate

--- agents/Group27/mcts/Node.py
+++ agents/Group27/mcts/Node.py
@@ -5,6 +5,7 @@ from src.Board import Board
 from src.Colour import Colour
 from src.Move import Move
 from math import inf, log, sqrt
+import copy
 
 def get_moves(board: Board, turn: int) -> list:
     moves = []
@@ -35,8 +36,16 @@ class Node:
         self.visits = 0
         self.value = 0.0
         self.player = player # false for 1, true for 2
+
+        # if self.parent:
+        #     self.untried_actions = copy.deepcopy(self.parent.actions)
+        #     self.untried_actions.remove(action)
+        # else:
+        #     self.untried_actions = get_moves(self.state, self.turn)    
+
+        # self.actions = self.untried_actions.copy()    
         self.untried_actions = get_moves(self.state, self.turn)
-        self.actions = get_moves(self.state, self.turn)
+        self.actions = self.untried_actions.copy()
         self.action = action
 
     def add_child(self, child_state: Board) -> Node:
